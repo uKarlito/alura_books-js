2b71d7cbd6b2865759592cdd848006b63d67ed65
uKarlito <dedepimpu@gmail.com>
2026-01-29 20:49:55 -0300

    Feat: método map

--- app/main.js
+++ app/main.js
@@ -1,31 +1,12 @@
-let livros = []
-const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+let livros = [];
+const endpointDaApi =
+  "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 
-getBuscarLibrosDaApi ()
-
-const elementoParaInserirLivros = document.getElementById('livros')
+getBuscarLibrosDaApi();
 
 async function getBuscarLibrosDaApi() {
-    const res = await fetch(endpointDaApi)
-    livros = await res.json()
-    console.table(livros)
-    exibirOsLivrosNaTela(livros)
-}
-
-function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-        `
-    });
+  const res = await fetch(endpointDaApi);
+  livros = await res.json();
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirOsLivrosNaTela(livrosComDesconto);
 }
